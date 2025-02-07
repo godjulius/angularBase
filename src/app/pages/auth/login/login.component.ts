278c7cbebd67144370fcd56c06ad125b932de261
duongnp <duongnp28072003@gmail.com>
2025-02-07 16:23:17 +0700

    Initial commit

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, MessageService} from 'primeng/api';
+import {ToastModule} from 'primeng/toast';
+import {SpeedDial} from 'primeng/speeddial';
+import {FormsModule} from '@angular/forms';
+import {Rating} from 'primeng/rating';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+    imports: [SpeedDial, ToastModule, FormsModule, Rating],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+    providers: [MessageService]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+    items!: MenuItem[] | null;
+    value: number = 3.5;
+    constructor(private messageService: MessageService) {}
 
+    ngOnInit() {
+        this.items = [
+            {
+                icon: 'pi pi-pencil',
+                command: () => {
+                    this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+                }
+            },
+            {
+                icon: 'pi pi-refresh',
+                command: () => {
+                    this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
+                }
+            },
+            {
+                icon: 'pi pi-trash',
+                command: () => {
+                    this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+                }
+            },
+            {
+                icon: 'pi pi-upload',
+                routerLink: ['/fileupload']
+            },
+            {
+                icon: 'pi pi-external-link',
+                target:'_blank',
+                url: 'http://angular.io'
+            }
+        ];
+    }
 }
